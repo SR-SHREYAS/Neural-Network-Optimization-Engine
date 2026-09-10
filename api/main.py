ed68d069bcb6eb7f0d25f75f4dc9203821b6ae08
SR-SHREYAS <shreyanshu2585.shreyas@gmail.com>
2026-09-10 22:48:56 +0530

    feat: add optimized model API

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-from api.routes.model import get_model_profile
+from api.routes.model import (
+    get_model_profile,
+    router as model_router,
+)
 from api.routes.optimize import router as optimize_router
+from api.routes.results import router as results_router
 
 
 app = FastAPI(
@@ -22,5 +26,13 @@ def model_profile():
 
 
 app.include_router(
+    model_router,
+)
+
+app.include_router(
     optimize_router,
+)
+
+app.include_router(
+    results_router,
 )
